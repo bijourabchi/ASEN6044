537c3da44973ddbfa1a32e3a07483dd8277c20c4
bijourabchi <bijo6049@colorado.edu>
2026-03-24 19:55:22 -0600

    HW3Q1 done

--- HW/HW3/H3Q1.m
+++ HW/HW3/H3Q1.m
@@ -31,12 +31,13 @@ P = P / sum(P(:));
 [X,Z] = meshgrid(z,x);
 
 figure
-surf(X,Z,P)
+surf(X,Z,P,'DisplayName','Prior')
 xlabel('z')
 ylabel('x')
 zlabel('p(x,z)')
 title('Joint Prior Distribution p(x,z)')
 shading interp
+legend('Location','best')
 
 %% Posterior Bayes Update
 
@@ -46,12 +47,13 @@ Ny = 3;
 L = likelihood(y(1),x,z);
 
 figure
-surf(X,Z,L)
+surf(X,Z,L,'DisplayName','Likelihood')
 xlabel('z')
 ylabel('x')
 zlabel('p(y|x,z)')
 title('Likelihood Dist p(y|x,z)')
 shading interp
+legend('Location','best')
 
 for i = 1:Ny
     Posterior = BayesUpdate(y(i),x,z,P, true,i);
@@ -75,15 +77,30 @@ for i = 1:Nx
     end
 end
 
+for i = 1:Ny-1
+    Posterior = BayesUpdate(y(i),x,z,P, false,i);
+    P = Posterior;
+    MAP = getMAP(P,x,z);
+    MMSE = getMMSE(P,x,z);
+end
+
 Ns = 500; % Sample Count
-% Since x/z are independant, we can sample from individual dist
-xi = 1 + 10*rand(Ns,1);
-zi = gamrnd(a,b,Ns,1);
+
+% Sample from prior
+cdf_prior = cumsum(P(:));
+random_values = rand(Ns, 1);
+sample_indices = arrayfun(@(t) find(cdf_prior >= t, 1, 'first'), random_values);
+[row_indices, col_indices] = ind2sub(size(P), sample_indices);
+
+xi = x(row_indices);
+zi = z(col_indices);
+
 w = zeros(1,500);
 
 for i = 1:Ns
 
-w(i) = likelihood(y(1),xi(i),zi(i))*likelihood(y(2),xi(i),zi(i))*likelihood(y(3),xi(i),zi(i));
+%w(i) = likelihood(y(1),xi(i),zi(i))*likelihood(y(2),xi(i),zi(i))*likelihood(y(3),xi(i),zi(i));
+w(i) = likelihood(y(3),xi(i),zi(i));
 
 end
 
@@ -92,12 +109,12 @@ w = w./sum(w); % Normalize
 % MMSE estimate
 IS_MMSE = [0;0];
 for i = 1:Ns
-    IS_MMSE = IS_MMSE + w(i)*[xi(i);zi(i)];
+    IS_MMSE = IS_MMSE + w(i)*[zi(i);xi(i)];
 end
 
 figure; hold on
-scatter3(zi, xi, w, 40, w, 'filled')
-plot3([IS_MMSE(1) IS_MMSE(1)],[IS_MMSE(2) IS_MMSE(2)],[0 max(w)],'k--')
+scatter3(xi, zi, w, 40, w, 'filled', 'DisplayName', 'Weighted Particles')
+plot3([IS_MMSE(1) IS_MMSE(1)],[IS_MMSE(2) IS_MMSE(2)],[0 max(w)],'k--', 'DisplayName', 'IS MMSE')
 xlim([1 14])
 ylim([0 30])
 xlabel('z')
@@ -108,6 +125,9 @@ title('Importance Sampling Weighted Particles')
 colorbar
 colormap turbo
 grid on
+legend('Location','best')
+view(3)
+exportgraphics(gcf, "IS.png", 'Resolution', 200);
 
 %% Helper Functions
 
@@ -148,14 +168,17 @@ if PLOT
     [X,Z] = meshgrid(z,x);
     figure
     hold on
-    surf(X,Z,Posterior)
-    scatter3(MAP(1),MAP(2),MAP(3),'rx')
-    plot3([MMSE(1) MMSE(1)],[MMSE(2) MMSE(2)],[0 MAP(3)],'k--')
-    xlabel('z')
-    ylabel('x')
+    surf(Z,X,Posterior,'DisplayName','Posterior')
+    scatter3(MAP(1),MAP(2),MAP(3),'rx','DisplayName','MAP')
+    plot3([MMSE(1) MMSE(1)],[MMSE(2) MMSE(2)],[0 MAP(3)],'k--','DisplayName','MMSE')
+    xlabel('x')
+    ylabel('z')
     zlabel('p(x,z|y)')
     title(sprintf('p(x,z | y_1...y_%d)',i))    
     shading interp
+    legend('Location','best')
+    view(3)
+    exportgraphics(gcf, sprintf('Measurment_%d',i) +  ".png", 'Resolution', 200);
 end
 end
 
@@ -164,22 +187,22 @@ function [MAP] = getMAP(P,x,z)
     [peak,idx] = max(P(:));
     [row, col] = ind2sub(size(P), idx);
     [X,Z] = meshgrid(z,x);
-    peakx = X(row,col);
-    peakz = Z(row,col);
+    map_z = X(row,col);
+    map_x = Z(row,col);
 
-    MAP = [peakx;peakz;peak];
+    MAP = [map_z;map_x;peak];
 
 end
 
 function [MMSE] = getMMSE(P,x,z)
 [X,Z] = meshgrid(z,x);
 
-x_mmse = sum(sum(P .* X));
-z_mmse = sum(sum(P .* Z));
+z_mmse = sum(sum(P .* X));
+x_mmse = sum(sum(P .* Z));
 
 [~, ix] = min(abs(x - x_mmse));
 [~, iz] = min(abs(z - z_mmse));
 
-MMSE = [x_mmse; z_mmse; P(ix,iz)];
+MMSE = [z_mmse; x_mmse; P(ix,iz)];
 
 end
